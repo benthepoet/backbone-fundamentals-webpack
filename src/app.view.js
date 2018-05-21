86fc5f77cd406c44cc29fde3aa02164b4ef0fe5e
Ben Hanna <benpaulhanna@gmail.com>
2018-05-21 17:39:57 +0000

    Added view file.

---
+++ src/app.view.js
@@ -0,0 +1,15 @@
+var Backbone = require('backbone');
+
+var viewOptions = {
+  el: 'body',
+  
+  initialize: function () {
+    this.render();
+  },
+  
+  render: function () {
+    this.$el.text('App Ready');
+  }
+};
+
+module.exports = Backbone.View.extend(viewOptions);
